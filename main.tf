d05b88d6a971d56dbd4502e8f627b6e6ee8bdcb2
BatuhanBugraAkca <batuhan.bba.06.06@gmail.com>
2025-05-21 17:22:26 -0500

    Initial commit - Terraform HW1

---
+++ main.tf
@@ -0,0 +1,46 @@
+# Terraform backend yapılandırması
+terraform {
+  backend "s3" {
+    bucket = "batuakca-terraform-backend"   # AWS Console'da elle oluşturduğun bucket
+    key    = "terraform.tfstate"        # State dosyasının ismi
+    region = "us-east-1"                # Bucket’ın bulunduğu AWS bölgesi
+  }
+}
+
+# AWS Provider Tanımı
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_s3_bucket" "first_bucket" {
+  bucket = "batuhanakcatentech-bucket-one"
+}
+
+resource "aws_s3_bucket" "second_bucket" {
+  bucket = "batuhanakcatentech-bucket-two"
+}
+
+resource "aws_s3_bucket" "third_bucket" {
+  bucket = "batuhanakcatentech-bucket-three"
+}
+resource "aws_vpc" "vpc_one" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "batuhan-vpc-one"
+  }
+}
+
+resource "aws_vpc" "vpc_two" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name = "batuhan-vpc-two"
+  }
+}
+
+resource "aws_vpc" "vpc_three" {
+  cidr_block = "10.2.0.0/16"
+  tags = {
+    Name = "batuhan-vpc-three"
+  }
+}
